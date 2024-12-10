ca721618ffc63c663f486863e491224cce2f9baa
altinaabazi <aa64958@ubt-uni.net>
2024-12-10 03:20:51 +0100

    Sidebar

--- E_Learning/obj/Debug/net7.0/E_Learning.AssemblyInfo.cs
+++ E_Learning/obj/Debug/net7.0/E_Learning.AssemblyInfo.cs
@@ -16,7 +16,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("E_Learning")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+974ff55f153cea000c4881c8b27e7848e2cbbdb3")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+1fe01519f9195de517c26afaa716c7262b7085b3")]
 [assembly: System.Reflection.AssemblyProductAttribute("E_Learning")]
 [assembly: System.Reflection.AssemblyTitleAttribute("E_Learning")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
