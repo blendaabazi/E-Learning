f93ca7a6acc9266f57c8765384b71e70fc9c9aa3
vleronemehmeti <vm64885@ubt-uni.net>
2024-12-15 19:51:02 +0100

    Dizajn i HomePage

--- E_Learning/obj/Debug/net7.0/E_Learning.AssemblyInfo.cs
+++ E_Learning/obj/Debug/net7.0/E_Learning.AssemblyInfo.cs
@@ -16,7 +16,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("E_Learning")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+f62681057d7870df53d61e68f9994084d602e0f3")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a41caa7ece806b514d584ab56c07bc251899597e")]
 [assembly: System.Reflection.AssemblyProductAttribute("E_Learning")]
 [assembly: System.Reflection.AssemblyTitleAttribute("E_Learning")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
